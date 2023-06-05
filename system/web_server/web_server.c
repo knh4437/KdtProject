98e068ad7cbe58a404bc837bd32877710192e840
knh4437 <knh4437@naver.com>
2023-06-05 14:21:15 +0000

    feat: clone + clone을 통한 프로세스 생성 격리 - lab19

--- system/web_server/web_server.c
+++ system/web_server/web_server.c
@@ -1,30 +1,61 @@
+#define _GNU_SOURCE
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/utsname.h>
+#include <sched.h>
+#include <signal.h>
 
 #include <system_server.h>
 #include <gui.h>
 #include <input.h>
 #include <web_server.h>
 
+static int child_func(void *arg) {
+
+    printf("[ Parent PID : %d ] Clone Succeed - child PID : %d\n", getppid(), getpid());
+
+    if (execl("/usr/local/bin/filebrowser", "filebrowser", "-p", "8282", (char *) NULL)) {
+        printf("execfailed\n");
+        return -1;
+    }
+    return 0;
+}
+
+#define STACK_SIZE (1024 * 1024) 
+
 int create_web_server()
 {
-    pid_t systemPid;
+    pid_t child_pid;
 
     printf("여기서 Web Server 프로세스를 생성합니다.\n");
 
-    /* fork + exec 를 이용하세요 */
-    /* exec으로 google-chrome-stable을 실행 하세요. */
+    
+    // fork를 clone + 새로운 namespace로 생성하세요.
+    char *stack = mmap(NULL, STACK_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_STACK, -1, 0);
+    if (stack == MAP_FAILED)
+        perror("mmap");
 
-    switch (systemPid = fork()) {
-    case -1:
-        printf("fork failed\n");
-    case 0:
-        if (execl("/usr/local/bin/filebrowser", "filebrowser", "-p", "8282", (char *) NULL)) {
-            printf("execfailed\n");
-        }
-        break;
-    default:
-        break;
-    }
+    child_pid = clone(child_func, stack + STACK_SIZE, CLONE_NEWUTS | CLONE_NEWIPC | CLONE_NEWPID | CLONE_NEWNS | SIGCHLD, NULL);
+    if (child_pid == -1)
+        perror("clone");
+    
+
+    // switch (systemPid = fork()) {
+    // case -1:
+    //     printf("fork failed\n");
+    // case 0:
+    //     if (execl("/usr/local/bin/filebrowser", "filebrowser", "-p", "8282", (char *) NULL)) {
+    //         printf("execfailed\n");
+    //     }
+    //     break;
+    // default:
+    //     break;
+    // }
 
     return 0;
 }
